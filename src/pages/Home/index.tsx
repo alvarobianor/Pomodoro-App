9d2d9e2b10ebb3f158e794ee92cd76959ef404a0
alvaro bianor <bianor.alvaro12@gmail.com>
2023-04-23 16:26:18 -0300

    Adding validation

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,7 @@
 import { Play } from 'phosphor-react'
 import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as zod from 'zod'
 
 import {
   CountdownContainer,
@@ -12,8 +14,18 @@ import {
   MinutesAmountInput,
 } from './styles'
 
+const newCicleFormValidationSchema = zod.object({
+  task: zod.string().min(1, 'Informe a tarefa.'),
+  minutesAmount: zod
+    .number()
+    .min(1, 'O mínimo é de 1 minuto.')
+    .max(90, 'O máximo é 90 minutos'),
+})
+
 export function Home() {
-  const { register, handleSubmit, watch } = useForm()
+  const { register, handleSubmit, watch } = useForm({
+    resolver: zodResolver(newCicleFormValidationSchema),
+  })
 
   const task = watch('task')
 
