b11b45d1107088c0656b625cf0d30d4fd010b642
Álvaro Bianor <bianor.alvaro12@gmail.com>
2024-01-21 10:38:09 -0300

    Update css

--- src/components/NewCyleForm/styles.ts
+++ src/components/NewCyleForm/styles.ts
@@ -13,27 +13,27 @@ export const TaskContainer = styled.div`
 `
 
 const BaseInput = styled.input`
-background: transparent;
-height: 2.5rem;
-border: 0;
-border - bottom: 2px solid ${(props) => props.theme['gray-500']};
-font - weight: bold;
-font - size: 1.125rem;
-padding: 0 0.5rem;
-color: ${(props) => props.theme['gray-100']};
+  background: transparent;
+  height: 2.5rem;
+  border: 0;
+  border-bottom: 2px solid ${(props) => props.theme['gray-500']};
+  font-weight: bold;
+  font-size: 1.125rem;
+  padding: 0 0.5rem;
+  color: ${(props) => props.theme['gray-100']};
   &:focus {
-  box - shadow: none;
-  border - color: ${(props) => props.theme['green-500']};
-}
+    box-shadow: none;
+    border-color: ${(props) => props.theme['green-500']};
+  }
   &::placeholder {
-  color: ${(props) => props.theme['gray-500']};
-}
+    color: ${(props) => props.theme['gray-500']};
+  }
 `
 
 export const TaskInput = styled(BaseInput)`
   flex: 1;
 
-  &:: -webkit - calendar - picker - indicator {
+  &::-webkit-calendar-picker-indicator {
     display: none !important;
   }
 `
